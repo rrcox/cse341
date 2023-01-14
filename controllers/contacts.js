dadfcfca124ee1ecd8c51d9ff0ff9221b1b1c754
rrcox <rob_cox@binary-development.com>
2023-01-14 14:38:34 -0500

    W02 fixes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,11 +1,21 @@
 const client = require('../model/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
     const  db = await client.getDb().db('cse341');
     const collection = db.collection('contacts')
     const cursor = collection.find();
-    const records = await cursor.toArray();
-    res.status(200).json(records);
+    const documents = await cursor.toArray();
+    res.status(200).json(documents);
   };
 
-  module.exports = { getAll };
+  const getOne = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id);
+    const  db = await client.getDb().db('cse341');
+    const collection = db.collection('contacts')
+    const cursor = collection.find({ _id: userId });
+    const document = await cursor.toArray();
+    res.status(200).json(document);
+  };
+
+  module.exports = { getAll, getOne };
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const contacts = require('../controllers/contacts');
 
 router.get('/', contacts.getAll);
+router.get('/:id', contacts.getOne);
 
 module.exports = router;
