60d5fe3c56159ba7cae739d8f22e0d8d7ede98ef
rrcox <rob_cox@binary-development.com>
2023-01-14 13:26:47 -0500

    W02 fixes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,10 +2,11 @@ const client = require('../model/connect');
 
 const getAll = async (req, res, next) => {
     const  db = await client.getDb().db('cse341');
-    const collection = db.collection('contacts')
-    const cursor = collection.find();
-    const records = await cursor.toArray();
-    res.status(200).json(records);
+    // const collection = db.collection('contacts')
+    // const cursor = collection.find();
+    // const records = await cursor.toArray();
+    // res.status(200).json(records);
+    res.send('working here...');
   };
 
   module.exports = { getAll };
